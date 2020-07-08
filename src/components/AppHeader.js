d9e67926d9d79d1741a62a912b93fee7b7b4cd13
Doug Stewart <contact@dougstwart.me>
2020-07-07 18:47:11 -0700

    :art: first pass logo

--- src/components/AppHeader.js
+++ src/components/AppHeader.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import styles from './AppHeader.module.css';
+import { ReactComponent as BaileLogo } from './../images/baile-logo.svg';
 
 const AppHeader = () => {
   return (
     <header className={styles.header}>
       <h1 className={styles.title}>
-        <strong>Baile</strong>{' '}
+        <BaileLogo className={styles.logo} /> <strong>Baile</strong>{' '}
         <span className={styles.define}>
           <em>/BAL-yeh/</em> (n.) Home
         </span>
